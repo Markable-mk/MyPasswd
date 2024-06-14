c61c9900b9fd5ec1ae538d985d8b7d2ae68bdf48
makuan <2777451153@qq.com>
2024-06-14 11:31:21 +0800

    feat: 20240614 初始化项目后端服务

---
+++ src/main/java/com/itmark/mypasswdbackend/MyPasswdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.itmark.mypasswdbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPasswdBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPasswdBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itmark/mypasswdbackend/MyPasswdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itmark.mypasswdbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPasswdBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
