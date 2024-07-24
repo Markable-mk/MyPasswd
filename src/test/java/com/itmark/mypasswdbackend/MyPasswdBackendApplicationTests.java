4a45a9548663f960a18b51ca49b4529255cf1f99
makuan <2777451153@qq.com>
2024-07-24 12:18:42 +0800

    feat: 2024072401 增加单元测试

--- src/test/java/com/itmark/mypasswdbackend/MyPasswdBackendApplicationTests.java
+++ src/test/java/com/itmark/mypasswdbackend/MyPasswdBackendApplicationTests.java
@@ -1,13 +1,21 @@
 package com.itmark.mypasswdbackend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
+//获取启动类，加载配置，寻找主配置启动类（被 @SpringBootApplication 注解的）
 @SpringBootTest
+//让JUnit运行Spring的测试环境,获得Spring环境的上下文的支持
+@RunWith(SpringRunner.class)
 class MyPasswdBackendApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("单元测试执行了");
     }
 
 }
